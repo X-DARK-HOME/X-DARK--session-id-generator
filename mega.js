d407b6ed7cb7bb3c874c997e4ea14e0f1ab6cef9
X-DARK-HOME <svd007psd003@gmail.com>
2024-11-04 13:31:10 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'svd007psd003@gmail.com',
+    password: '@fakepw//12345',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
